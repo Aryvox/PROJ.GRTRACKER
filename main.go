e52c3d7932e683243cdf5c264d84387dd561f4a2
Aryvox <johan.daumas@ynov.com>
2025-01-06 09:08:00 +0100

    PROJ.GRTRACKER: Release

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"crypto/md5"
-	"fmt"
+	"api/routes"
+	"api/templates"
 )
 
 func main() {
-	/* templates.InitTemplates()
-	routes.InitServe() */
+	templates.InitTemplates()
+	routes.InitServe()
 
 	// value hash
-	ts := 19
+	/*ts := 19
 	public_key := "1e2027484c06e5cc452a3b8de485b482"
 	private_key := "35eb641e8155bef52b1fd281b32d16611b1841c7"
 	token := fmt.Sprintf("%d%s%s", ts, private_key, public_key)
@@ -18,5 +18,5 @@ func main() {
 	h := md5.New()
 	h.Write([]byte(token))
 	hash := fmt.Sprintf("%x", h.Sum(nil))
-	fmt.Println(hash)
+	fmt.Println(hash)*/
 }
